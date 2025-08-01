da4c0f21a3af7bf48529c9b003d13ba6e0b37a03
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-08-01 11:37:59 +0900

    #1 [FEATURE] Shop 엔티티 및 repository 생성

---
+++ src/main/java/com/example/easybooking/shop/Shop.java
@@ -0,0 +1,14 @@
+package com.example.easybooking.shop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Shop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+}
