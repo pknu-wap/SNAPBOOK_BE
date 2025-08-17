fe3f6df892ed6d7a87a2119fb6047e300ce1669b
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-08-17 12:33:05 +0900

    #1 [CHORE] 프로젝트 세팅

---
+++ src/main/java/com/example/easybooking/EasybookingApplication.java
@@ -0,0 +1,11 @@
+package com.example.easybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasybookingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/easybooking/EasybookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasybookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
