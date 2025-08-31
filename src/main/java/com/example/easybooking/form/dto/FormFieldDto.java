f87505226e42b33afc87b73d1132635ba726c7f7
seobeeeee1001 <wjsbdcindsu@pukyong.ac.kr>
2025-08-31 23:53:22 +0900

    #10 [FIX] Json 직렬화 실패 해결

--- src/main/java/com/example/easybooking/form/dto/FormFieldDto.java
+++ src/main/java/com/example/easybooking/form/dto/FormFieldDto.java
@@ -1,11 +1,14 @@
 package com.example.easybooking.form.dto;
 
-import com.example.easybooking.form.domain.type.FieldType;
-import lombok.Builder;
-
 import java.util.List;
 
+import com.example.easybooking.form.domain.type.FieldType;
+
+import lombok.Builder;
+import lombok.Data;
+
 @Builder
+@Data
 public class FormFieldDto {
     private String fieldId;
     private String label;
